e53e9f4757a3eca4a249fae377225864592a48f4
Nazar T <60976828+Fenixkriss@users.noreply.github.com>
2020-05-25 15:40:11 +0300

    Update task2_Black_Sheep.py

--- Viach/task2_Black_Sheep.py
+++ Viach/task2_Black_Sheep.py
@@ -7,6 +7,6 @@ lst = [0, 8, 2, 50, 13, 6, 34]
 # 011 101 110 111 sum >= 2 : all 'odd', target is 'even'
 test_sum = lst[0] % 2 + lst[1] % 2 + lst[2] % 2
 target = 1 if test_sum < 2 else 0
-result = [x for x in lst if x%2 == target]
+result = [x for x in lst if x%2 == target][0]
 print('Innitial list:\n', lst)
-print('Black Sheep is', result[0])
+print('Black Sheep is', result)
