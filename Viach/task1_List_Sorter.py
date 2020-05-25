f64be9864c9263cffd198c17263b90bb9949bf6d
Nazar T <60976828+Fenixkriss@users.noreply.github.com>
2020-05-25 15:16:01 +0300

    Update task1_List_Sorter.py

--- Viach/task1_List_Sorter.py
+++ Viach/task1_List_Sorter.py
@@ -3,7 +3,7 @@
 # 	Initial list:     yellow  white 2 5  green  red    6 1 
 # 	Final sorted list: green   red   1 2  white  yellow 5 6
 
-def List_Sorter_Ligth(lst_inn):
+def list_sorter_ligth(lst_inn):
 	# Main function - light version, only strings and numbers in list
 	lst_out, lst_of_str, lst_of_num  = [], [], []
 	# Sepparator str/num
@@ -19,7 +19,7 @@ def List_Sorter_Ligth(lst_inn):
 	return lst_out	
 
 
-def List_Sorter_Heavy(lst_inn):
+def list_sorter_heavy(lst_inn):
 	# Main function - resistant to defferent type of element exceptions
 	# If unsupported elements (nested lists or tuples of dict) found they leave untouched 
 	lst_out, lst_of_str, lst_of_num  = [], [], []
@@ -48,11 +48,11 @@ def List_Sorter_Heavy(lst_inn):
 # --- main ---
 
 initial_list = ['yellow','white', 2, 5,'green','red', 6, 1, 9 , 'apple', 0] 
-sorted_list = List_Sorter_Ligth(initial_list)
+sorted_list = list_sorter_ligth(initial_list) #function call
 print('Innitial list:\n', initial_list)			
 print('Sorted list:\n', sorted_list)
 
 initial_list_2 = ['yellow','white', 2, 5,('black',7),'red', [5,'x'], 1, 9 , 'apple', 0] 
-sorted_list = List_Sorter_Heavy(initial_list_2)
+sorted_list = list_sorter_heavy(initial_list_2) #function call
 print('Innitial list:\n', initial_list_2)			
 print('Sorted list:\n', sorted_list)
